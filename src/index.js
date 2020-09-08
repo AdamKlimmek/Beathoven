1d0edad1b926838405357cd3929fa5c0408bad18
Adam Klimmek <aklimmek@mac.com>
2020-09-08 13:19:59 -0700

    initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+    var para = document.createElement("P");                       // Create a <p> node
+    var t = document.createTextNode("This is a paragraph.");      // Create a text node
+    para.appendChild(t);                                          // Append the text to <p>
+    document.getElementById("root").appendChild(para);    
+});
