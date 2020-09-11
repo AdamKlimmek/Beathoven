7c4f3e683577108bb2df78ef298d84c75b3f8070
Adam Klimmek <aklimmek@mac.com>
2020-09-11 16:31:04 -0700

    testing audio playback on live hosting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import * as Tone from 'tone';
 
+let audioUrl1 = require(`../dist/samples/sample_01.wav`);
+
 window.addEventListener('DOMContentLoaded', () => {
 
     function generateSequencer() {
@@ -83,7 +85,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const soundKitA = [];
     function generateSoundKitOne() {
         for (let i = 1; i <= 8; i++) {
-            let sound = new Tone.Player(`../dist/samples/sample_0${i}.wav`).toDestination();
+            let sound = new Tone.Player(audioUrl1).toDestination();
             soundKitA.push(sound);
         }
     }
